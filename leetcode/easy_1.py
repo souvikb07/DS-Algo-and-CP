d63f7f39c7dff6f8ed7351f88fdba8e368d8b010
Souvik Banerjee <souvikbanerjee@UCLP2186.local>
2020-10-12 10:36:58 +0530

    second solution

--- leetcode/easy_1.py
+++ leetcode/easy_1.py
@@ -30,7 +30,7 @@ Constraints:
 Only one valid answer exists.
 
 """
-
+# Solution 1
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         output_dict = {}
@@ -40,3 +40,13 @@ class Solution:
                 return [output_dict[next_num],i]
             else:
                 output_dict[num] = i
+
+# Solution 2
+"""
+Approach 2: Two-pass Hash Table
+To improve our run time complexity, we need a more efficient way to check if the complement exists in the array. If the complement exists, we need to look up its index. What is the best way to maintain a mapping of each element in the array to its index? A hash table.
+
+We reduce the look up time from O(n)O(n) to O(1)O(1) by trading space for speed. A hash table is built exactly for this purpose, it supports fast look up in near constant time. I say "near" because if a collision occurred, a look up could degenerate to O(n)O(n) time. But look up in hash table should be amortized O(1)O(1) time as long as the hash function was chosen carefully.
+
+A simple implementation uses two iterations. In the first iteration, we add each element's value and its index to the table. Then, in the second iteration we check if each element's complement (target - nums[i]target−nums[i]) exists in the table. Beware that the complement must not be nums[i]nums[i] itself!
+"""
